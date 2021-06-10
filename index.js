72fd0a9fc35bc31e0402754aec6b4104ae8c34de
choiree <riririgood@naver.com>
2021-06-10 23:01:24 +0900

    created js

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const todayDay = today.querySelector(".today-day")//요일
 const todayDate = today.querySelector(".today-date")//날짜
 const todayMonth = today.querySelector(".today-month")//월
 const todayYear = today.querySelector(".today-year")//년도
+const first = document.querySelector(".first");
 
 
+function calender(){
+    const date = new Date();
+    const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);//이달의 1일
+    const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);//0일은없기에 하루전날을 반환함
+    document.write(firstDay);
+    document.write(lastDay);
 
+    first.innerText= firstDay;
+}
 
 
 function currentDate(){
@@ -21,10 +30,12 @@ function currentDate(){
     todayDate.innerText = date;
     todayMonth.innerText = month;
     todayYear.innerText = year;
+
 }
 
 function init(){
     currentDate();
     setInterval(currentDate, 86400000);
+    calender();
 }
 init();
